a598ffa97a1dad4f2c2ca85a393cfdae7092abd7
flond7 <elisa.utilivenza@gmail.com>
2022-03-04 09:48:11 +0100

    added djongo to settings

--- mainProject/mainProject/settings.py
+++ mainProject/mainProject/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = 'mainProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+""" SQLITE3 DB - STANDARD OPTION
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} 
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'w-database',
     }
 }
 
