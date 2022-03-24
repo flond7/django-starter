4a39e646676a67d70734ffed01e10254f88d7eb2
flond7 <elisa.utilivenza@gmail.com>
2022-03-24 08:52:50 +0100

    added id to models and blank options

--- mainProject/api/models.py
+++ mainProject/api/models.py
@@ -1,35 +1,41 @@
 # from django.db import models
 from djongo import models
 from django import forms
+import uuid
 from api.modelsConstants import *
 
 # Create your models here.
 class appointment(models.Model):
   _id = models.ObjectIdField()
+  appointmentId = models.UUIDField(primary_key=False, unique=True, default=uuid.uuid4, editable=False)
   appointmentPlace  = models.CharField(
     max_length = 2,
     choices = PLACE_CHOICES,
     default = 'p1',
+    blank=True
   )
-  appointmentDay = models.DateTimeField()
+  appointmentDay = models.DateTimeField(blank=True)
   personOne = models.CharField(
     max_length = 2,
     choices = PERSON_CHOICES,
     default = 'p1',
+    blank=True
   )
   personTwo = models.CharField(
     max_length = 2,
     choices = PERSON_CHOICES,
     default = 'p1',
+    blank=True
   )
   volunteer = models.CharField(max_length=100, blank=True, default='')
-  durationTime = models.DurationField()
+  durationTime = models.DurationField(blank=True)
   type = models.CharField(
     max_length = 2,
     choices = APPOINTMENT_TYPE_CHOICES,
     default = 't1',
+    blank=True
   )
-  report = models.TextField()
+  report = models.TextField(blank=True)
 
   class Meta:
     ordering = ['appointmentDay']
@@ -39,23 +45,27 @@ class appointment(models.Model):
 
 class author(models.Model):
   _id = models.ObjectIdField()
+  authorId = models.UUIDField(primary_key=False, unique=True, default=uuid.uuid4, editable=False)
   name = models.CharField(max_length=100, blank=True, default='')
   surname = models.CharField(max_length=100, blank=True, default='')
-  yearsOld = models.IntegerField()
+  yearsOld = models.IntegerField(blank=True, )
   education = models.CharField(
     max_length = 7,
     choices = AUTHOR_EDUCATION_CHOICES,
     default = 'e0',
+    blank=True, 
   )
   job = models.CharField(
     max_length = 7,
     choices = AUTHOR_JOB_CHOICES,
     default = 'j0',
+    blank=True, 
   )
   relationship = models.CharField(
     max_length = 7,
     choices = AUTHOR_RELATIONSHIP_CHOICES,
     default = 'r0',
+    blank=True, 
   )
 
   class Meta:
@@ -65,9 +75,10 @@ class author(models.Model):
     return self.title
 
 class path(models.Model):
-  _id = models.ObjectIdField()
-  start = models.DateTimeField()
-  end = models.DateTimeField()
+  _id = models.ObjectIdField() 
+  pathId = models.UUIDField(primary_key=False, unique=True, default=uuid.uuid4, editable=False)
+  start = models.DateField()
+  end = models.DateField(blank=True, )
   status = models.CharField(
     max_length = 7,
     choices = PATH_STATUS_CHOICES,
@@ -89,21 +100,23 @@ class path(models.Model):
 class woman(models.Model):
   name = models.CharField(max_length=100, blank=True, default='')
   surname = models.CharField(max_length=100, blank=True, default='')
-  birthdate = models.DateTimeField()
-  timestamp = models.DateTimeField(auto_now_add=True)
+  email = models.EmailField(blank=True)
+  test = models.CharField(max_length=100, blank=True, default='')
+  tel = models.CharField(max_length=20, blank=True, default='')
+  birthdate = models.DateField()
+  createdAt = models.DateTimeField(auto_now_add=True)
+  lastAppointment = models.DateField(auto_now=True)
   citizenship = models.CharField(
     max_length = 2,
     choices = CITIZENSHIP_CHOICES,
     default = 'IT',
   )
-  path = models.EmbeddedField(
+  path = models.ArrayField(
     model_container=path
   )
 
-  report = models.TextField()
-
   class Meta:
-    ordering = ['birthdate']
+    ordering = ['lastAppointment']
 
   def __str__(self):
     return self.title
